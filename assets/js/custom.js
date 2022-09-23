182c5e9b3008aad4a7c0c90c88bc5715a35acfe2
Ahmedkhnunar <ahmedkhanunar@gmail.com>
2022-09-23 18:28:36 +0500

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,23 @@
-
+function AjaxRequest(params,method='GET')
+{
+    let urlParams = "?";
+    $.each(params,function(k,v){
+      urlParams =  urlParams + k +"=" + v +"&";
+    });
+    urlParams = urlParams.slice(0, -1)
+    var url = "http://localhost/knowledgecity/api/" + urlParams;
+    var response;
+    $.ajax(
+    {
+      method:method,
+      url:url,
+      contentType: "application/json;",
+      data:JSON.stringify(params),
+      datatype:"json",
+      async:false,
+      success:function(res, status, xhttp){
+        response = JSON.parse(res);
+      }
+    });
+    return response;
+}
